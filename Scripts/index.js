e4baf57ec9ac53eb836ef56d5385a55203e2429d
Alexandr Ostapenko <ostap1320@gmail.com>
2022-01-15 13:39:27 +0300

    добавление блоков

---
+++ Scripts/index.js
@@ -0,0 +1,19 @@
+function view (id){
+   let i =  document.getElementsByClassName(id)
+    getComputedStyle(i).style
+
+}
+
+/*
+
+
+            function viewDiv(){
+                document.getElementsByClassName("firstque").style.display = "block;"
+
+
+                
+            document.getElementById("firstque").style.display = "block";};
+        
+
+
+*/
