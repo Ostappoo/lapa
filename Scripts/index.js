ce505abab0f798ce93b33b6de545b783452ed425
Alexandr Ostapenko <ostap1320@gmail.com>
2022-01-24 20:25:25 +0300

    ass

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,19 +1,42 @@
-function view (id){
-   let i =  document.getElementsByClassName(id)
-    getComputedStyle(i).style
-
+function Menu (){
+        let imenu = document.getElementById('servicesblock');
+        if (imenu.style.display == "flex"){
+            imenu.style.display = "none"; 
+        }
+        else{
+            imenu.style.display = "flex";}
 }
-
-/*
-
-
-            function viewDiv(){
-                document.getElementsByClassName("firstque").style.display = "block;"
+ 
 
 
-                
-            document.getElementById("firstque").style.display = "block";};
-        
+    let i = 1;
+    for(let li of carousel.querySelector('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
 
+    /* конфигурация */
+    let width = 150; // ширина картинки
+    let count = 2; // видимое количество изображений
 
-*/
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // положение ленты прокрутки
+
+    carousel.querySelector('.prev').onclick = function() {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
