78481d1a851a887ae3852f4f574450865b393bb8
Alexandr Ostapenko <ostap1320@gmail.com>
2022-01-25 20:42:16 +0300

    all body

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,74 +7,10 @@ function Menu (){
             imenu.style.display = "flex";}
 }
 
-function anser1(){
-    let emenu = document.getElementById('answer1');
-    if (emenu.style.display == "block"){
-        emenu.style.display = "none"; 
-    }
-    else{
-        emenu.style.display = "block";}
-}
-function anser2(){
-    let emenu = document.getElementById('answer2');
-    if (emenu.style.display == "block"){
-        emenu.style.display = "none"; 
-    }
-    else{
-        emenu.style.display = "block";}
-}
-function anser3(){
-    let emenu = document.getElementById('answer3');
-    if (emenu.style.display == "block"){
-        emenu.style.display = "none"; 
-    }
-    else{
-        emenu.style.display = "block";}
-}
-function anser4(){
-    let emenu = document.getElementById('answer4');
-    if (emenu.style.display == "block"){
-        emenu.style.display = "none"; 
-    }
-    else{
-        emenu.style.display = "block";}
-}
-
-
-
-
-
-/*
-    let i = 1;
-    for(let li of carousel.querySelector('li')) {
-      li.style.position = 'relative';
-      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-      i++;
-    }
-
-    // конфигурация 
-    let width = 150; // ширина картинки
-    let count = 2; // видимое количество изображений
-
-    let list = carousel.querySelector('ul');
-    let listElems = carousel.querySelectorAll('li');
-
-    let position = 0; // положение ленты прокрутки
-
-    carousel.querySelector('.prev').onclick = function() {
-      // сдвиг влево
-      position += width * count;
-      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-      position = Math.min(position, 0)
-      list.style.marginLeft = position + 'px';
-    };
-
-    carousel.querySelector('.next').onclick = function() {
-      // сдвиг вправо
-      position -= width * count;
-      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-      position = Math.max(position, -width * (listElems.length - count));
-      list.style.marginLeft = position + 'px';
-    };
-
-    */
+function menuid (element_id){   
+    let obj = document.getElementById(element_id); 
+        if (obj.style.display != "block") { 
+            obj.style.display = "block"; 
+        }
+    else obj.style.display = "none"; 
+}       
