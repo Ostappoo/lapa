abb31ff902f7368719eaf42f8e94daa71454f495
Alexandr Ostapenko <ostap1320@gmail.com>
2022-01-30 22:16:52 +0300

    swapbutton upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,4 @@
-function Menu (){
+/*function Menu (){
         let imenu = document.getElementById('servicesblock');
         if (imenu.style.display == "flex"){
             imenu.style.display = "none"; 
@@ -6,7 +6,9 @@ function Menu (){
         else{
             imenu.style.display = "flex";}
 }
+*/
 
+/* Вопросы и ответы  */
 function menuid (element_id){   
     let obj = document.getElementById(element_id); 
         if (obj.style.display != "block") { 
@@ -14,3 +16,27 @@ function menuid (element_id){
         }
     else obj.style.display = "none"; 
 }       
+
+
+/* кнопка */
+let button = document.getElementById('button-container')
+let ButtDoge = document.getElementById('ButtDoge')
+let ButtCat = document.getElementById('ButtCat')
+let buttonState = true;
+
+button.addEventListener('click', function(){
+  if (buttonState) {
+    document.getElementById("my-button").style.transform = "translateX(112px)"; 
+    buttonState = false;
+    ButtDoge.innerText = 'Кошка'
+    ButtCat.innerText = 'Собака'
+    ButtCat.style.transform = "translateX(-112px)";
+  } else {
+    document.getElementById("my-button").style.transform = "translateX(0px)";
+    buttonState = true;
+    ButtDoge.innerText = 'Собака'
+    ButtCat.innerText = 'Кошка'
+    ButtCat.style.transform = "translateX(0px)";
+  }
+    
+})
