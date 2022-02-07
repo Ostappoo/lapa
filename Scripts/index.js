054939cd6e80d631e477af3cec13e73dbd56478c
Alexandr Ostapenko <ostap1320@gmail.com>
2022-02-08 00:46:02 +0300

    fix

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,37 +1,3 @@
-/*function Menu (){
-        let imenu = document.getElementById('servicesblock');
-        if (imenu.style.display == "flex"){
-            imenu.style.display = "none"; 
-        }
-        else{
-            imenu.style.display = "flex";}
-}
-*/
-
-/* кнопка */
-/*
-let button = document.getElementById('button-container')
-let ButtDoge = document.getElementById('ButtDoge')
-let ButtCat = document.getElementById('ButtCat')
-let buttonState = true;
-
-button.addEventListener('click', function(){
-  if (buttonState) {
-    document.getElementById("my-button").style.transform = "translateX(112px)"; 
-    buttonState = false;
-    ButtDoge.innerText = 'Кошка'
-    ButtCat.innerText = 'Собака'
-    ButtCat.style.transform = "translateX(-112px)";
-  } else {
-    document.getElementById("my-button").style.transform = "translateX(0px)";
-    buttonState = true;
-    ButtDoge.innerText = 'Собака'
-    ButtCat.innerText = 'Кошка'
-    ButtCat.style.transform = "translateX(0px)";
-  }
-    
-})
-*/
 
 /* Вопросы и ответы  */
 function displayBlock (elId){
@@ -44,10 +10,27 @@ function workerToDo(id,len=4) {
   for(let i = 1; i<=len; i++) document.getElementById('worker-block'+i).style.display = 'none'
    let activeworker = document.getElementById(id)
    activeworker.style.display = 'block'
-  //setInterval()
-  //setTimeout()
-  //WindowTimers
 }
+
+let toogleDown = (item, itemSvg) => {
+  let itemId = document.getElementById(item)
+  const ch = itemId.clientHeight,
+    sh = itemId.scrollHeight,
+    isCollapsed = !ch,
+    noHeightSet = !itemId.style.height;
+    itemId.style.height = (isCollapsed || noHeightSet ? sh : 0) + "px";
+  svgBlock(itemSvg)
+};
+
+
+function svgBlock (item){
+  let itemDom = document.getElementById(item);
+  itemDom.style.display = itemDom.style.display == 'block' ? 'none' : 'block'
+  let itemDom1 = document.getElementById(item +1);
+  itemDom1.style.display = itemDom.style.display == 'block' ? 'none' : 'block'
+}
+
+
 
 
 const swiper = new Swiper('.swiper', {
@@ -56,7 +39,6 @@ const swiper = new Swiper('.swiper', {
   slidesPerGroup: 1,
   resistanceRatio: 1,
   autoHeight: false,
-
   pagination: {
       el: '.swiper-pagination',
       type: 'fraction',
@@ -67,26 +49,23 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swapbuttons-prev',
   },
   breakpoints: {
-    320:{
-      slidesPerView: 1,
-      spaceBetween: 80,
+  320:{
+    slidesPerView: 1,
+    spaceBetween: 80,
     },
-    480:{
-      slidesPerView: 1.2,
-      spaceBetween: 0,
+  480:{
+    slidesPerView: 1.2,
+    spaceBetween: 0,
     },
-    992:{
-      slidesPerView: 2,
-      spaceBetween: 0,
-     
+  992:{
+    slidesPerView: 2,
+    spaceBetween: 0,
     },
-    1440:{
-      slidesPerView: 3,
-      spaceBetween: 0,
-      
+  1440:{
+    slidesPerView: 3,
+    spaceBetween: 0,
     },
-    
   }
 }); 
-swiper.width = 450;
+
 
